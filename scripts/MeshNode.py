175429927f26be306a2a1c7145e8904014750b13
kash <kashishgarg9@gmail.com>
2024-10-08 14:56:22 -0400

    Save point, meshnode architecture is clearer now

--- scripts/MeshNode.py
+++ scripts/MeshNode.py
@@ -2,40 +2,75 @@ import meshtastic
 import meshtastic.serial_interface
 import time
 from pubsub import pub
+import sys
 from termcolor import colored, cprint
 
+
+import lorem
+
+
+from PacketHeader import PacketHeader
 
 class MeshNode:
 
     def __init__(self, id):
         self.meshtastic_id = id 
 
+        print(f"Initializing MeshNode with ID: {id}")
+        self.packet_header = PacketHeader()
+
         # Byte Sizes for each component of the Packet Header
-        self.packet_header = {}
+        # TODO: Make this an interation on the dictionary for robust error checking
+        print("============================================================")
+        print(f"Number of Bytes for the Packet Header: {self.packet_header.packet_sizes['packet_header_size']}")
+        print(f"    Number of Bytes for the Hash: {self.packet_header.packet_sizes['hash_size']}")
+        print(f"    Number of Bytes for the Time Stamp: {self.packet_header.packet_sizes['timestamp_size']}")
+        print(f"    Number of Bytes for the Number of Total Packets: {self.packet_header.packet_sizes['num_total_packet_size']}")
+        print(f"    Number of Bytes for the Packet Sequence Number: {self.packet_header.packet_sizes['packet_sequence_size']}")
+        print(f"    Number of Bytes for the Packet Type: {self.packet_header.packet_sizes['packet_type_size']}")
 
+        self.max_string_size= 237 - self.packet_header.packet_sizes["packet_header_size"] 
+        print("============================================================")
+        print(f"Maximum str size for msgs: {self.max_string_size}")
 
-        self.packet_header_size = ( self.hash_size + 
-                                    self.timestamp_size + 
-                                    self.num_total_packet_size +
-                                    self.packet_sequence_size +
-                                    self.packet_type_size )
+        print("Done Initializing!")
+        print(".")
+        print(".")
+        print(".")
 
-        print(f"Packet Header Size: {self.packet_header_size} Bytes")
-        print(f"    Hash Size: {self.hash_size}")
-        print(f"    Time Stamp Size: {self.timestamp_size}")
-        print(f"    Total Number of Packet Size: {self.num_total_packet_size}")
-        print(f"    Packet Sequence Number Size: {self.packet_sequence_size}")
-        print(f"    Packet Type Size: {self.packet_type_size}")
-        # Byte Size Limit for String Message
+    def CreateMsg(self, string_input: str):
+        string_input = string_input.encode("utf-8")
+        sizeof_str = len(string_input)
+        print(string_input)
+        print(f"Size of Input String: {sizeof_str} bytes")
 
-        # pub.subscribe(self.packet_respond, "meshtastic.receive")
-
-
+        # Container for either single or list of Strings, depending on following conditional
+        output_string = []
+        if sizeof_str > self.max_string_size:
+            print(colored("String input is greater than max string size! Performing fragmentation...", 'yellow'))
+            output_string = self.SeparateString(string_input)
+        else:
+            output_string.append(string_input)
+        
     def SeparateString(self, string_input: str) -> [str]:
-        pass
+        """ Takes a string larger than the self.max_string_size and fragments 
+        it out to a list of substrings
+
+        Parameters
+        ----------
+        string_input
+            String encoded as utf-8 greater than self.max_string_size
+
+        Returns
+        -------
+        [str]
+            List of strings, each ready for packetization
+        """
+
+
 
     def CreatePacket(self, string_input:str) -> str:
-        """Converts a string into the desired packet for Meshtastic experiments
+        """ Converts a string into the desired packet for Meshtastic experiments
             The max size a string can be 237 bytes - our custom packet header 
             size
             Hash Length - 32 bits : 4 bytes
@@ -65,6 +100,6 @@ class MeshNode:
 
 if __name__ == '__main__':
     mn = MeshNode("a")
-    string_input = "hello"
-    mn.CreatePacket(string_input)
+    string_input = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    mn.CreateMsg(string_input)
     
--- scripts/PacketHeader.py
+++ scripts/PacketHeader.py
@@ -1,6 +1,11 @@
-from num import Enum
+from enum import Enum
 
-class PacketHeader(self):
+class PacketType(Enum):
+    REQ = 1 # Req from Robot to Base (LLM Input)
+    REP = 2 # Rep from LLM to go from Base to Robot
+    FDBK = 3  # Continous messages to indicate current status of Robot
+
+class PacketHeader:
     def __init__(self):
         # All header components are strings
         self.hash = None
@@ -16,30 +21,43 @@ class PacketHeader(self):
         self.packet_sequence_size = 1
         self.packet_type_size = 1
 
-        self.packet_component_sizes = {
+
+        # Total Packet Size
+        self.packet_header_size = ( self.hash_size + 
+                                    self.timestamp_size + 
+                                    self.num_total_packet_size +
+                                    self.packet_sequence_size +
+                                    self.packet_type_size )
+
+        self.packet_sizes = {
             "hash_size": self.hash_size,
             "timestamp_size": self.timestamp_size,
             "num_total_packet_size": self.num_total_packet_size,
             "packet_sequence_size": self.packet_sequence_size,
-            "packet_type_size": self.packet_type_size
+            "packet_type_size": self.packet_type_size,
+            "packet_header_size": self.packet_header_size
         }
     
-    def SetHash(self, hash)
+    def SetHash(self, hash):
         # TODO Perform Stringification 
         self.hash = hash
 
-    def SetTimestamp(self, time_stamp)
+    def SetTimestamp(self, time_stamp):
         # TODO Perform Stringification 
-        self.hash = hash
+        self.time_stamp = time_stamp
 
-    def SetNumTotalPackets(self, num_total_packets)
+    def SetNumTotalPackets(self, num_total_packets):
         # TODO Perform Stringification 
-        self.hash = hash
+        self.num_total_packets = num_total_packets
 
-    def SetPacketSequence(self, packet_sequence)
+    def SetPacketSequence(self, packet_sequence):
         # TODO Perform Stringification 
-        self.hash = hash
+        self.packet_sequence = packet_sequence
 
-    def SetPacketType(self, packet_type)
+    def SetPacketType(self, packet_type):
         # TODO Perform Stringification 
-        self.hash = hash
+        self.packet_type = packet_type
+
+if __name__ == '__main__':
+    packet_header = PacketHeader()
+    print(packet_header.packet_sizes["packet_header_size"])
